aa486171ced47571039bcb87d94ae30cd6be9787
Markus Zapke-Gründemann <markus@transcode.de>
2015-11-16 22:16:00 +0100

    Added deploy helper script for faster testing

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd
+. venv/bin/activate
+cd /src
+pip install -r requirements.txt
+./manage.py migrate
+./manage.py migrate --database=newsdb
+./manage.py createsuperuser --username admin --email admin@example.com
+./manage.py loaddata recipes initial_data
+./manage.py collectstatic --noinput
+cp -R media /home/vagrant
+sudo chown -R www-data: /home/vagrant/media
+sudo service apache2 stop
+sudo service apache2 start
