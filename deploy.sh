b8e261f714405ec5a3f2f934f69dd2bfec6a10ef
Markus Zapke-Gründemann <markus@transcode.de>
2015-11-20 10:14:46 +0100

    Reduced output generated by collectstatic command

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ pip install -r requirements.txt
 ./manage.py migrate --database=newsdb
 ./manage.py createsuperuser --username admin --email admin@example.com
 ./manage.py loaddata recipes initial_data
-./manage.py collectstatic --noinput
+./manage.py collectstatic -v0 --noinput
 cp -R media /home/vagrant
 sudo chown -R www-data: /home/vagrant/media
 sudo service apache2 stop
