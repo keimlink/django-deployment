0e4b8be23b0cc81c3e16ce0ff1ce87907d05ab73
Markus Zapke-Gründemann <markus@transcode.de>
2016-01-15 20:19:02 +0100

    Revised Bash test deployment script

--- test_deployment.sh
+++ test_deployment.sh
@@ -1,26 +1,31 @@
-#!/bin/bash
-# Bash script to test deployment
+#!/usr/bin/env bash
+# Test Django deployment
+
+set -o errexit
+set -o pipefail
+set -o verbose
+
 cd
-echo "Activating virtualenv"
+# Activate virtualenv
 . venv/bin/activate
 cd /src
-echo "Installing requirements"
+# Install requirements
 pip install -r requirements.txt
-echo "Running migrations"
+# Run migrations
 ./manage.py migrate
 ./manage.py migrate --database=newsdb
-echo "Running deployment checks"
+# Run deployment checks
 ./manage.py check --deploy
-echo "Creating new superuser 'admin'"
+# Create new superuser 'admin'
 ./manage.py createsuperuser --username admin --email admin@example.com
-echo "Loading initial data"
+# Load initial data
 ./manage.py loaddata recipes initial_data
-echo "Collecting static files"
+# Collect static files
 ./manage.py collectstatic -v0 --noinput
-echo "Copying media files"
+# Copy media files
 sudo rm -fr /home/vagrant/media
 cp -R media /home/vagrant
 sudo chown -R www-data: /home/vagrant/media
-echo "Restarting Apache"
+# Restart Apache
 sudo service apache2 stop
 sudo service apache2 start
